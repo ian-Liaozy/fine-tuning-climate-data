a2d7d1ac1cca38ea1196ac018845ff6135a369c7
ian-Liaozy <zl3057@nyu.edu>
2025-03-06 16:39:24 -0500

    avoid re process exist txt

--- data_processing.py
+++ data_processing.py
@@ -10,6 +10,7 @@ os.makedirs(output_dir, exist_ok=True)
 file_list = os.listdir(input_dir)
 random.shuffle(file_list)
 # split file list
+txt_list = os.listdir(output_dir)
 
 train_list = file_list[:int(len(file_list) * 0.9)]
 test_list = file_list[int(len(file_list) * 0.9):]
@@ -17,6 +18,8 @@ test_list = file_list[int(len(file_list) * 0.9):]
 for file in train_list:
     if file.endswith(".pdf"):
         with open(input_dir + file, "rb") as f:
+            if file.replace(".pdf", ".txt") in txt_list:
+                continue
             reader = PdfReader(f)
             text = ""
             for page in reader.pages:
@@ -28,6 +31,8 @@ for file in train_list:
 for file in test_list:
     if file.endswith(".pdf"):
         with open(input_dir + file, "rb") as f:
+            if file.replace(".pdf", ".txt") in txt_list:
+                continue
             reader = PdfReader(f)
             text = ""
             for page in reader.pages:
