f2ee12dcc27fa4768c8a125d554cf4529e487add
ian-Liaozy <zl3057@nyu.edu>
2025-03-09 00:15:11 -0500

    fix ValueError: The model did not return a loss from the inputs

--- fine_tuning.py
+++ fine_tuning.py
@@ -17,7 +17,12 @@ tokenizer.pad_token = tokenizer.eos_token
 
 
 def tokenize_function(examples):
-    return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
+    tokenized_inputs = tokenizer(
+        examples["text"], truncation=True, padding="max_length", max_length=512
+    )
+    tokenized_inputs["labels"] = tokenized_inputs["input_ids"].copy()
+    return tokenized_inputs
+
 
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 train_dataset = tokenized_datasets["train"]
