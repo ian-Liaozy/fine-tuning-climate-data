569e8e7a57641fa7b51f7e1f0f391d9348c33e2b
ian-Liaozy <zl3057@nyu.edu>
2025-03-10 13:56:56 -0400

    reduce steps

--- fine_tuning.py
+++ fine_tuning.py
@@ -3,6 +3,8 @@ from peft import get_peft_model, LoraConfig, TaskType, prepare_model_for_kbit_tr
 # from bitsandbytes import quantize
 from datasets import load_dataset
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 dataset_path = "/scratch/zl3057/processed_txt"
 
 dataset = load_dataset("text", data_files={
@@ -24,7 +26,7 @@ def tokenize_function(examples):
     return tokenized_inputs
 
 
-tokenized_datasets = dataset.map(tokenize_function, batched=True, load_from_cache_file=False)
+tokenized_datasets = dataset.map(tokenize_function, batched=True, load_from_cache_file=False, num_proc=1)
 train_dataset = tokenized_datasets["train"]
 test_dataset = tokenized_datasets["test"]
 training_args = TrainingArguments(
@@ -38,10 +40,10 @@ training_args = TrainingArguments(
     dataloader_num_workers=4,
     output_dir="./checkpoints/",
     warmup_steps=5,
-    max_steps=50,
+    max_steps=25,
     eval_strategy="steps",
     eval_steps=25,
-    save_steps=50,
+    save_steps=25,
     logging_steps=5,
     learning_rate=1e-4,
     report_to="none",
