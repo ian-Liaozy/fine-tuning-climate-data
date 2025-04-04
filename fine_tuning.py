774beb49f1afd90c340c9929cf0a7f334e10750f
ian-Liaozy <zl3057@nyu.edu>
2025-04-04 00:43:52 -0400

    debug pipeline mode

--- fine_tuning.py
+++ fine_tuning.py
@@ -145,7 +145,7 @@ def main():
         dummy_input = torch.randint(0, tokenizer.vocab_size, (4, 42), device=device)
 
         if rank == 0:
-            schedule.step((dummy_input,))
+            schedule.step(dummy_input)
         else:
             schedule.step()
     else:
