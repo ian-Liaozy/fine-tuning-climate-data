d5b5bcf83ff7bc54c5aa7b02c76b30500340e1bd
ian-Liaozy <zl3057@nyu.edu>
2025-04-04 12:43:40 -0400

    debug pipeline parall

--- fine_tuning.py
+++ fine_tuning.py
@@ -64,7 +64,7 @@ def get_model(model_name, parallel_mode="none", devices=None):
         if rank == 0:
             model.model.layers = layers[:half]
             # model.model.norm = None
-            model.lm_head = None
+            # model.lm_head = None
         else:
             model.model.embed_tokens = None
             model.model.layers = layers[half:]
